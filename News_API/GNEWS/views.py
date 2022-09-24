6642979dac038252924e7798c0e9f44f93f4f4aa
Saad Salman <61458557+Saad-Salman@users.noreply.github.com>
2022-09-25 00:24:43 +0500

    Add files via upload

--- News_API/GNEWS/views.py
+++ News_API/GNEWS/views.py
@@ -1,4 +1,5 @@
 from http.client import HTTPResponse
+import mimetypes
 from django.shortcuts import render
 import requests
 API_KEY = '540f1899cdde617624a21697bbd4f36d'
@@ -6,20 +7,17 @@ Key_word = 'IT'
 
 
 # Create your views here.
-
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 def home(request):
     
-    url = f'https://gnews.io/api/v4/search?q={Key_word}&token={API_KEY}'
+    url = f'https://gnews.io/api/v4/search?q={Key_word}&token={API_KEY}&max=20'
     response = requests.get(url)
-    data = response.json()
-    print(data)
+    data = response.json()['articles']
+    
+    return JsonResponse(data, safe=False)
 
-    context = {
-        'data':data
-    }
-
-    return render(request,'GNEWS/home.html',context)
 
 
 
